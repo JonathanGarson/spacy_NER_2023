ec1e4efe30edeaa7c95d50a8b2b8f6d5ef225535
cthiounn2 <thiounn.conrad@gmail.com>
2023-10-03 13:28:36 +0000

    init from cookiecutter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A NLP model to extract numbers from business agreements',
+    author='Jonathan Garson',
+    license='MIT',
+)
